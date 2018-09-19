a0b0206978a840440d148fafcf30abeb75e09118
Patrik Segedy <psegedy@redhat.com>
2018-09-19 17:27:45 +0300

    pylint

--- webapp/test/test_utils.py
+++ webapp/test/test_utils.py
@@ -1,9 +1,13 @@
+"""Unit tests for utils module."""
+
 import unittest
 import utils
-from datetime import datetime
+
 
 class TestUtils(unittest.TestCase):
-    """Unit test utils"""
+    """Unit test utils."""
+
     def test_join_pkgname(self):
+        """Test joining package name"""
         pkg_name = utils.join_packagename("test", "2", "1.2", "4.el7", "x86_64")
         self.assertEqual(pkg_name, "test-2:1.2-4.el7.x86_64")
